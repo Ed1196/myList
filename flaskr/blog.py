8758b6bc2a92eb125a5ed60386fce9b0ba2557bd
Ariel <ariel@Ariels-MBP.fios-router.home>
2018-09-05 22:42:39 -0400

    Second commit

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,16 +4,24 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
+from flaskr.auth import load_logged_in_user
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
 #SHOW posts.
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
+    #posts = db.execute(
+    #    'SELECT p.id, title, body, created, author_id, username, complete
+    #    ' FROM post p JOIN user u ON p.author_id = u.id'
+    #    ' ORDER BY created DESC'
+    #    ).fetchall()
+
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username, complete '
+        ' SELECT p.id, p.title, p.body, p.created, p.author_id, u.username, p.complete, u.id '
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
         ).fetchall()
